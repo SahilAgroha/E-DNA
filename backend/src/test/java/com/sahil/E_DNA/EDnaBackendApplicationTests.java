95304cabaa17e9c4e6d9d29a32df18e6eb3c5551
Sahil <sahil.sheoran.agroha@gmail.com>
2025-09-16 16:09:07 +0530

    second commit

---
+++ backend/src/test/java/com/sahil/E_DNA/EDnaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sahil.E_DNA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EDnaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
