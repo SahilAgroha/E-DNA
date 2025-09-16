da0a0b89faaa065aa4b9033900217acc4e24fe5c
Sahil <sahil.sheoran.agroha@gmail.com>
2025-09-16 16:28:14 +0530

    backend

--- backend/src/main/java/com/sahil/E_DNA/controller/home.java
+++ backend/src/main/java/com/sahil/E_DNA/controller/home.java
@@ -1,4 +1,13 @@
 package com.sahil.E_DNA.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class home {
+
+    @GetMapping
+    public String home(){
+        return "Hello";
+    }
 }
