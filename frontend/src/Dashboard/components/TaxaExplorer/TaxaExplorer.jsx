7a281a37a4dbc81baad63f411ed5fde3d0dcf707
Sahil <sahil.sheoran.agroha@gmail.com>
2025-09-26 16:40:09 +0530

    Filnal Version 1.0

--- frontend/src/Dashboard/components/TaxaExplorer/TaxaExplorer.jsx
+++ frontend/src/Dashboard/components/TaxaExplorer/TaxaExplorer.jsx
@@ -76,7 +76,7 @@ const TaxaExplorer = () => {
 
     return (
       <Box>
-        <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-6">
+        <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-6">
           {currentItems.map((taxa, index) => {
             const theme = colorThemes[index % colorThemes.length];
             return (
